4f4c875b415151ed3eae56570cda7f9ca60589f9
Avijit Roy <aviroy2005@gmail.com>
2025-07-13 23:31:21 +0600

    Dart

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,27 @@
+void main() {
+  print("Hello Dart!");
+  /* 1
+  2
+  3
+  */
+
+  //Variable declaration and Data Types
+//var => Variable
+  var x = 40.34;  //float
+  var y = 60; //int
+
+  var z = x + y;
+  print("The sum of x and y is: $z");
+  // print(z);
+
+  var str = "Avijit"; //String
+  var str2 = "Roy";
+  print(str + str2);
+
+  var isBangladeshi = true; //boolean
+  print("Is Avijit Bangladeshi? $isBangladeshi");
+
+
+
+}
+
