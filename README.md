e0840925c858f8f43a6aca89bbb0926260c66475
Avijit Roy <aviroy2005@gmail.com>
2025-07-14 00:38:28 +0600

    Dart

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 A sample command-line application with an entrypoint in `bin/`, library code
 in `lib/`, and example unit test in `test/`.
+
+Youtube Tutorial link: https://youtu.be/_8Q5cwfvi64?si=aBHBDtPONTJ9TDjV
+1st dat: >1:14:46   | July 14 - 2025    | 12:37 AM
--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,5 @@
 void main() {
+  /*
   print("Hello Dart!");
   /* 1
   2
@@ -21,7 +22,7 @@ void main() {
   var isBangladeshi = true; //boolean
   print("Is Avijit Bangladeshi? $isBangladeshi");
 
-  //List
+  //List      লিস্ট এ একাধিক ভ্যালু রাখা যায় যার index থাকে। এর এলিমেন্ট গুলো একই টাইপের হতে হবে না। লিস্ট এ একটি একক ভ্যালু থাকে। 
   var city = ['Dhaka', 'Khulna', 'Rajshahi', 'Chittagong', 'Sylhet'];
 
   print(city[0]); //Accessing first element
@@ -32,5 +33,49 @@ void main() {
   print("Total number of cities: ${city.length}"); //Length of the list
   print(city); //Print the entire list
 
-  
+
+
+  //DART MAP      ম্যাপ এ key-value পেয়ার থাকে। ম্যাপ এ key গুলো ইউনিক হতে হবে। এর দুইটি ভ্যালু থাকে। একটি key এবং আরেকটি value।
+  var student = {'name': 'Avijit', 'age': 21, 'brunch': 'Science'}; //Map with key-value pairs
+  print(student['name']); //Accessing value by key
+  print(student['age']); //Accessing value by key
+  print(student['brunch']); //Accessing value by key
+  print("Total number of keys in student map: ${student.length}"); //Length of the map
+  print(student); //Print the entire map
+
+  //DART OPERATORS
+  var a = 10;
+  var b = 20;
+
+  print("Addition: ${a + b}"); //Addition
+  print("Subtraction: ${a - b}"); //Subtraction
+  print("Multiplication: ${a * b}"); //Multiplication
+  print("Division: ${a / b}"); //Division
+  print("Modulus: ${a % b}"); //Modulus
+  // print(a++); //Postfix Increment
+  // print(b--); //Postfix Decrement
+  print("Increment: ${++a}"); //Increment
+  print("Decrement: ${--b}"); //Decrement
+  print("Exponentiation: ${a * a}"); //Exponentiation (a^2)
+  print("Floor Division: ${a ~/ b}"); //Floor Division (integer division)
+  print("Negation: ${-a}"); //Negation (unary minus)
+  print("Equality: ${a == b}"); //Equality check
+  print("Inequality: ${a != b}"); //Inequality check
+  print("Greater than: ${a > b}"); //Greater than check
+  print("Less than: ${a < b}"); //Less than check
+  print("Greater than or equal to: ${a >= b}"); //Greater than or equal to check
+  print("Less than or equal to: ${a <= b}"); //Less than or equal to check
+
+
+  //DART CONSTANTS
+  // ডার্টে কনস্ট্যান্ট মান গুলো পরিবর্তন করা যায় না। কনস্ট্যান্ট মান গুলো প্রোগ্রাম চলাকালীন সময়ে অপরিবর্তিত থাকে।
+  const pi = 3.1416; //Constant value
+  // const বা  final দিয়ে কনস্ট্যান্ট ভ্যালু ডিক্লেয়ার করা যায়। const দিয়ে ডিক্লেয়ার করা মান গুলো কম্পাইল টাইমে নির্ধারিত হয়। আর final দিয়ে ডিক্লেয়ার করা মান গুলো রানটাইমে নির্ধারিত হয়।
+  final gravity = 9.81; //Final value (can only be set once)
+  print("Value of pi: $pi");
+  print("Value of gravity: $gravity");
+ */
+//LIST PROPERTIES     
+
+
 }
