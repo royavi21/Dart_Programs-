b009634ba2323778a186717f1a55663f0285af45
Avijit Roy <aviroy2005@gmail.com>
2025-07-13 23:37:15 +0600

    Dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -6,8 +6,8 @@ void main() {
   */
 
   //Variable declaration and Data Types
-//var => Variable
-  var x = 40.34;  //float
+  //var => Variable
+  var x = 40.34; //float
   var y = 60; //int
 
   var z = x + y;
@@ -21,7 +21,16 @@ void main() {
   var isBangladeshi = true; //boolean
   print("Is Avijit Bangladeshi? $isBangladeshi");
 
+  //List
+  var city = ['Dhaka', 'Khulna', 'Rajshahi', 'Chittagong', 'Sylhet'];
 
+  print(city[0]); //Accessing first element
+  print(city[1]); //Accessing second element
+  print(city[2]); //Accessing third element
+  print(city[3]); //Accessing fourth element
+  print(city[4]); //Accessing fifth element
+  print("Total number of cities: ${city.length}"); //Length of the list
+  print(city); //Print the entire list
 
+  
 }
-
